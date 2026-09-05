b9ec2b626dd98e55447c04694bda126288e08613
simonasr <7496885+simonasr@users.noreply.github.com>
2026-09-05 21:56:17 +0300

    chore: prepare 4.3.0 release

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybls21",
-    version="4.2.2",
+    version="4.3.0",
     author="Julius Vitkauskas",
     author_email="zadintuvas@gmail.com",
     description="An api allowing control of AC state (temperature, on/off, speed) of an Blauberg S21 device locally over TCP",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jvitkauskas/pybls21",
+    url="https://github.com/simonasr/pybls21",
     packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=["pymodbus>=3.11.2,<4.0"],
     classifiers=[
